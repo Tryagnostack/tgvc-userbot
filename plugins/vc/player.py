c9da9138e508d730a50e627f66b30d2e6b8b0efd
Tryagnostack <63946350+Tryagnostack@users.noreply.github.com>
2021-07-10 16:41:52 +0530

    yt attempt1

--- plugins/vc/player.py
+++ plugins/vc/player.py
@@ -41,12 +41,26 @@ from datetime import datetime, timedelta
 import ffmpeg
 from pyrogram import Client, filters, emoji
 from pyrogram.methods.messages.download_media import DEFAULT_DOWNLOAD_DIR
-from pyrogram.types import Message
+from pyrogram.types import Message, Audio
 from pytgcalls import GroupCall
+from youtube_dl import YoutubeDL
+from PIL import Image
 
 DELETE_DELAY = 8
 DURATION_AUTOPLAY_MIN = 10
 DURATION_PLAY_HOUR = 3
+MUSIC_MAX_LENGTH = 10800
+TG_THUMB_MAX_LENGTH = 320
+
+REGEX_SITES = (
+    r"^((?:https?:)?\/\/)"
+    r"?((?:www|m)\.)"
+    r"?((?:youtube\.com|youtu\.be))"
+    r"(\/)([-a-zA-Z0-9()@:%_\+.~#?&//=]*)([\w\-]+)(\S+)?$"
+)
+REGEX_EXCLUDE_URL = (
+    r"\/channel\/|\/playlist\?list=|&list=|\/sets\/"
+)
 
 USERBOT_HELP = f"""{emoji.LABEL}  **Common Commands**:
 __available to group members of current voice chat__
@@ -512,3 +526,125 @@ async def _delay_delete_messages(messages: tuple, delay: int):
     await asyncio.sleep(delay)
     for m in messages:
         await m.delete()
+
+@Client.on_message(main_filter
+                   & filters.regex(REGEX_SITES)
+                   & ~filters.regex(REGEX_EXCLUDE_URL))
+async def music_downloader(client: Client, message: Message):
+    await _fetch_and_send_music(client, message)
+
+
+async def _fetch_and_send_music(client: Client, message: Message):
+    # await message.reply_chat_action("typing")
+    processing = await message.reply_text("Processing Youtube video...")
+    try:
+        ydl_opts = {
+            'format': 'bestaudio',
+            'outtmpl': '%(title)s - %(extractor)s-%(id)s.%(ext)s',
+            'writethumbnail': True
+        }
+        ydl = YoutubeDL(ydl_opts)
+        info_dict = ydl.extract_info(message.text, download=False)
+
+        if info_dict['duration'] > MUSIC_MAX_LENGTH:
+            readable_max_length = str(timedelta(seconds=MUSIC_MAX_LENGTH))
+            inform = ("This won't be downloaded because its audio length is "
+                      "longer than the limit `{}` which is set by the bot"
+                      .format(readable_max_length))
+            await _reply_and_delete_later(message, inform,
+                                          DELAY_DELETE_INFORM)
+            return
+        # d_status = await message.reply_text("Downloading...", quote=True,
+        #                                     disable_notification=True)
+        ydl.process_info(info_dict)
+        audio_file = ydl.prepare_filename(info_dict)
+        task = asyncio.create_task(_upload_audio(client, message, info_dict,
+                                                 audio_file))
+        # await message.reply_chat_action("upload_document")
+        # await d_status.delete()
+        while not task.done():
+            await asyncio.sleep(4)
+            # await message.reply_chat_action("upload_document")
+        # await message.reply_chat_action("cancel")
+        audio = task.result()
+        message.audio = audio
+        await processing.delete()
+
+        await play_track(client, message)
+
+        if message.chat.type == "private":
+            await message.delete()
+    except Exception as e:
+        await message.reply_text(repr(e))
+
+
+def _youtube_video_not_music(info_dict):
+    if info_dict['extractor'] == 'youtube' \
+            and 'Music' not in info_dict['categories']:
+        return True
+    return False
+
+
+async def _reply_and_delete_later(message: Message, text: str, delay: int):
+    reply = await message.reply_text(text, quote=True)
+    await asyncio.sleep(delay)
+    await reply.delete()
+
+
+async def _upload_audio(client: Client, message: Message, info_dict, audio_file):
+    basename = audio_file.rsplit(".", 1)[-2]
+    if info_dict['ext'] == 'webm':
+        audio_file_opus = basename + ".opus"
+        ffmpeg.input(audio_file).output(audio_file_opus, codec="copy").run()
+        os.remove(audio_file)
+        audio_file = audio_file_opus
+    thumbnail_url = info_dict['thumbnail']
+    if os.path.isfile(basename + ".jpg"):
+        thumbnail_file = basename + ".jpg"
+    else:
+        thumbnail_file = basename + "." + \
+            _get_file_extension_from_url(thumbnail_url)
+    squarethumb_file = basename + "_squarethumb.jpg"
+    make_squarethumb(thumbnail_file, squarethumb_file)
+    webpage_url = info_dict['webpage_url']
+    title = info_dict['title']
+    caption = f"<b><a href=\"{webpage_url}\">{title}</a></b>"
+    duration = int(float(info_dict['duration']))
+    performer = info_dict['uploader']
+    res = await client.send_audio(chat_id=-1001313909409,
+                                    audio=audio_file,
+                                    caption=caption,
+                                    duration=duration,
+                                    performer=performer,
+                                    title=title,
+                                    parse_mode='HTML',
+                                    thumb=squarethumb_file)
+    for f in (audio_file, thumbnail_file, squarethumb_file):
+        os.remove(f)
+    return res.audio
+
+
+def _get_file_extension_from_url(url):
+    url_path = urlparse(url).path
+    basename = os.path.basename(url_path)
+    return basename.split(".")[-1]
+
+
+def make_squarethumb(thumbnail, output):
+    """Convert thumbnail to square thumbnail"""
+    # https://stackoverflow.com/a/52177551
+    original_thumb = Image.open(thumbnail)
+    squarethumb = _crop_to_square(original_thumb)
+    squarethumb.thumbnail((TG_THUMB_MAX_LENGTH, TG_THUMB_MAX_LENGTH),
+                          Image.ANTIALIAS)
+    squarethumb.save(output)
+
+
+def _crop_to_square(img):
+    width, height = img.size
+    length = min(width, height)
+    left = (width - length) / 2
+    top = (height - length) / 2
+    right = (width + length) / 2
+    bottom = (height + length) / 2
+    return img.crop((left, top, right, bottom))
